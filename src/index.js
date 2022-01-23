4ee2872d5e0cb2dc7c624c1f9ef09100238aa02e
Karadesh <akamalavr@gmail.com>
2022-01-23 15:50:46 +0300

    lesson_1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import getProductList from "./mock/data.js";
+import renderGoodsList from "./showcase.js";
+import "./index.css";
+
+const productList = getProductList(20);
+
+renderGoodsList(productList);
